e5d2b0e6c030b03c75b618d4de0d266ff9bf007c
delaval <delaval.htps@gmail.com>
2021-08-24 17:50:24 +0200

    update mpd & sql

--- sql_archi/paymybuddy.sql
+++ sql_archi/paymybuddy.sql
@@ -1,11 +1,25 @@
 
-CREATE TABLE daily_invoice (
-                daily_invoice_id INT AUTO_INCREMENT NOT NULL,
-                date_invoice DATETIME NOT NULL,
-                price_ht DECIMAL(8,2) NOT NULL,
-                price_ttc DECIMAL(8,2) NOT NULL,
-                taxe_percent DECIMAL(3,2) NOT NULL,
-                PRIMARY KEY (daily_invoice_id)
+CREATE TABLE application_identifier (
+                application_identifier_id INT AUTO_INCREMENT NOT NULL,
+                email VARCHAR(30) NOT NULL,
+                password VARCHAR(30) NOT NULL,
+                PRIMARY KEY (application_identifier_id)
+);
+
+
+CREATE TABLE social_network_identifer (
+                social_network_id INT AUTO_INCREMENT NOT NULL,
+                username_login VARCHAR(30) NOT NULL,
+                user_password VARCHAR(30) NOT NULL,
+                PRIMARY KEY (social_network_id)
+);
+
+
+CREATE TABLE card_bank (
+                numberCard INT NOT NULL,
+                card_code INT NOT NULL,
+                date_expiration_card DATE NOT NULL,
+                PRIMARY KEY (numberCard)
 );
 
 
@@ -16,61 +30,34 @@ CREATE TABLE application_account (
 );
 
 
-CREATE TABLE user (
-                user_id INT NOT NULL,
-                lastName VARCHAR(30) NOT NULL,
-                firstName VARCHAR(30) NOT NULL,
-                email VARCHAR(30) NOT NULL,
-                password VARCHAR(30) NOT NULL,
-                number_account INT NOT NULL,
-                PRIMARY KEY (user_id)
-);
-
-
-CREATE TABLE application_identifier (
-                application_identifier_id INT AUTO_INCREMENT NOT NULL,
-                user_password VARCHAR(30) NOT NULL,
-                username_login VARCHAR(30) NOT NULL,
-                user_id INT NOT NULL,
-                PRIMARY KEY (application_identifier_id, user_password, username_login)
-);
-
-
-CREATE TABLE social_network_identifer (
-                social_network_id INT AUTO_INCREMENT NOT NULL,
-                username_login VARCHAR(30) NOT NULL,
-                user_password VARCHAR(30) NOT NULL,
-                user_id INT NOT NULL,
-                PRIMARY KEY (social_network_id)
-);
-
-
 CREATE TABLE bank_account (
                 number_account INT NOT NULL,
                 iban VARCHAR(34) NOT NULL,
-                user_id INT NOT NULL,
+                balance DECIMAL(8,2) NOT NULL,
+                numberCard INT NOT NULL,
                 PRIMARY KEY (number_account)
 );
 
 
-CREATE TABLE chekbook (
-                number_checkbook INT NOT NULL,
-                number_account INT NOT NULL,
-                PRIMARY KEY (number_checkbook, number_account)
-);
-
-
-CREATE TABLE card_bank (
-                numberCard INT NOT NULL,
-                number_account INT NOT NULL,
-                date_expiration_card DATE NOT NULL,
-                PRIMARY KEY (numberCard, number_account)
+CREATE TABLE user (
+                user_id INT AUTO_INCREMENT NOT NULL,
+                lastName VARCHAR(30) NOT NULL,
+                firstName VARCHAR(30) NOT NULL,
+                address VARCHAR(100) NOT NULL,
+                zip INT NOT NULL,
+                city VARCHAR(30) NOT NULL,
+                phone INT NOT NULL,
+                number_application_account INT NOT NULL,
+                number_bank_account INT NOT NULL,
+                social_network_id INT NOT NULL,
+                application_identifier_id INT NOT NULL,
+                PRIMARY KEY (user_id)
 );
 
 
 CREATE TABLE connection_user (
                 user_id INT NOT NULL,
-                user_connection_id INT AUTO_INCREMENT NOT NULL,
+                user_connection_id INT NOT NULL,
                 PRIMARY KEY (user_id, user_connection_id)
 );
 
@@ -83,36 +70,50 @@ CREATE TABLE transaction (
                 commision_percent DECIMAL(3,2) NOT NULL,
                 type_transaction VARCHAR(20) NOT NULL,
                 user_id INT NOT NULL,
-                application_number_account INT NOT NULL,
-                bank_number_account INT NOT NULL,
                 user_connection_id INT NOT NULL,
                 PRIMARY KEY (transaction_id)
 );
 
 
-CREATE TABLE line_transaction (
+CREATE TABLE invoice (
+                daily_invoice_id INT AUTO_INCREMENT NOT NULL,
+                date_invoice DATETIME NOT NULL,
+                price_ht DECIMAL(8,2) NOT NULL,
+                price_ttc DECIMAL(8,2) NOT NULL,
+                taxe_percent DECIMAL(3,2) NOT NULL,
                 transaction_id INT NOT NULL,
-                daily_invoice_id INT NOT NULL,
-                time_transaction TIME NOT NULL,
-                PRIMARY KEY (transaction_id, daily_invoice_id)
+                user_id INT NOT NULL,
+                PRIMARY KEY (daily_invoice_id)
 );
 
 
-ALTER TABLE line_transaction ADD CONSTRAINT daily_invoice_line_transaction_fk
-FOREIGN KEY (daily_invoice_id)
-REFERENCES daily_invoice (daily_invoice_id)
+ALTER TABLE user ADD CONSTRAINT application_identifier_user_fk
+FOREIGN KEY (application_identifier_id)
+REFERENCES application_identifier (application_identifier_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE user ADD CONSTRAINT social_network_identifer_user_fk
+FOREIGN KEY (social_network_id)
+REFERENCES social_network_identifer (social_network_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE bank_account ADD CONSTRAINT card_bank_bank_account_fk
+FOREIGN KEY (numberCard)
+REFERENCES card_bank (numberCard)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
 ALTER TABLE user ADD CONSTRAINT application_account_user_fk
-FOREIGN KEY (number_account)
+FOREIGN KEY (number_application_account)
 REFERENCES application_account (number_account)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE transaction ADD CONSTRAINT application_account_transaction_fk
-FOREIGN KEY (application_number_account)
-REFERENCES application_account (number_account)
+ALTER TABLE user ADD CONSTRAINT bank_account_user_fk
+FOREIGN KEY (number_bank_account)
+REFERENCES bank_account (number_account)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
@@ -128,45 +129,9 @@ REFERENCES user (user_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE transaction ADD CONSTRAINT user_transaction_fk
+ALTER TABLE invoice ADD CONSTRAINT user_invoice_fk
 FOREIGN KEY (user_id)
 REFERENCES user (user_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE bank_account ADD CONSTRAINT user_bank_account_fk
-FOREIGN KEY (user_id)
-REFERENCES user (user_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE social_network_identifer ADD CONSTRAINT user_social_network_identifer_fk
-FOREIGN KEY (user_id)
-REFERENCES user (user_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE application_identifier ADD CONSTRAINT user_application_identifier_fk
-FOREIGN KEY (user_id)
-REFERENCES user (user_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE card_bank ADD CONSTRAINT bank_account_card_bank_fk
-FOREIGN KEY (number_account)
-REFERENCES bank_account (number_account)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE chekbook ADD CONSTRAINT bank_account_chekbook_fk
-FOREIGN KEY (number_account)
-REFERENCES bank_account (number_account)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE transaction ADD CONSTRAINT bank_account_transaction_fk
-FOREIGN KEY (bank_number_account)
-REFERENCES bank_account (number_account)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
@@ -176,7 +141,7 @@ REFERENCES connection_user (user_id, user_connection_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE line_transaction ADD CONSTRAINT transaction_line_transaction_fk
+ALTER TABLE invoice ADD CONSTRAINT transaction_daily_invoice_fk
 FOREIGN KEY (transaction_id)
 REFERENCES transaction (transaction_id)
 ON DELETE NO ACTION
