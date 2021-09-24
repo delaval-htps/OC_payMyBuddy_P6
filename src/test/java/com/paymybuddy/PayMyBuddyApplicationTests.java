2c20782a5c177dbdb48cb1551fa7de459aed4393
Delaval Dorian <delaval.htps@gmail.com>
2021-09-24 13:18:53 +0200

    update name of app

--- src/test/java/com/paymybuddy/PayMyBuddyApplicationTests.java
+++ src/test/java/com/paymybuddy/PayMyBuddyApplicationTests.java
@@ -1,13 +1,20 @@
 package com.paymybuddy;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.paymybuddy.controllers.LoginController;
 
 @SpringBootTest
-class PayMyBuddy1ApplicationTests {
+class PayMyBuddyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private LoginController controllerUnderTest;
+
+  @Test
+  void contextLoads() {
+    assertThat(controllerUnderTest).isNotNull();
+  }
 
 }
