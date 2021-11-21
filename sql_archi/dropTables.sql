d423d6d3589c7547000c09b8bb149e350262b86d
Delaval Dorian <delaval.htps@gmail.com>
2021-11-21 13:33:02 +0100

    creation of script dropTable.sql

---
+++ sql_archi/dropTables.sql
@@ -0,0 +1,9 @@
+drop table if exists paymybuddy.invoice;
+drop table if exists paymybuddy.transaction;
+drop table if exists paymybuddy.connection_user;
+drop table if exists paymybuddy.user;
+drop table if exists paymybuddy.social_network_identifier	;
+drop table if exists paymybuddy.application_identifier;
+drop table if exists paymybuddy.application_account;
+drop table if exists paymybuddy.bank_account;
+drop table if exists paymybuddy.card_bank;
