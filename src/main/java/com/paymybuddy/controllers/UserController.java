581f4316cb3319053939cbe6cdd70e50d130c8d8
delaval-htps <delaval.htps@gmail.com>
2021-12-28 19:09:30 +0100

    update config custom login for usernamePasswordAuthenticationToken
    change logincontroller to userController
    update login.html & PayMybuddySecurityConfig
    to match with custom login et authentication

--- src/main/java/com/paymybuddy/controllers/UserController.java
+++ src/main/java/com/paymybuddy/controllers/UserController.java
@@ -1,15 +1,22 @@
 package com.paymybuddy.controllers;
 
+import com.paymybuddy.security.services.SecurityService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class LoginController {
+public class UserController {
 
-  @GetMapping("/myLoginPage")
+  @Autowired
+  private SecurityService securityService;
+
+  @GetMapping("/showLoginPage")
   public String showLoginPage() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
@@ -17,5 +24,14 @@ public class LoginController {
     } else {
       return "redirect:/home";
     }
+  }
+
+  @PostMapping("/authenticateUser")
+  public String login(String username, String password) {
+    boolean responseLogin = securityService.login(username, password);
+    if (responseLogin) {
+      return "home";
+    }
+    return "login";
   }
 }
