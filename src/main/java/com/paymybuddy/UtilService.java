59fa0f6e6b74a50b7549217d580669969c96b797
Delaval Dorian <delaval.htps@gmail.com>
2022-03-06 11:49:52 +0100

    correction of creation Account Number

--- src/main/java/com/paymybuddy/UtilService.java
+++ src/main/java/com/paymybuddy/UtilService.java
@@ -8,21 +8,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class UtilService {
 
+  public static String getRandomIban() throws NoSuchAlgorithmException {
 
-
-  public static String getRandomAccountNumber() throws NoSuchAlgorithmException {
     Random rand = SecureRandom.getInstanceStrong();
-
-    String startNumber = "FR";
-
     StringBuilder numberAccounString = new StringBuilder();
 
-    numberAccounString.append(startNumber);
+    numberAccounString.append("IBAN FR");
     numberAccounString.append(String.format("%02d", rand.nextInt(99)));
-    for (int i = 0; i < 3; i++) {
+    numberAccounString.append(" ");
+
+    for (int i = 0; i < 5; i++) {
+      numberAccounString.append(String.format("%04d", rand.nextInt(9999)));
       numberAccounString.append(" ");
-      numberAccounString.append(String.format("%04d", rand.nextInt(99)));
     }
+
+    numberAccounString.append(String.format("%03d", rand.nextInt(999)));
+
+    return numberAccounString.toString();
+
+  }
+
+  public static String getRandomAccountNumber() throws NoSuchAlgorithmException {
+
+    Random rand = SecureRandom.getInstanceStrong();
+    StringBuilder numberAccounString = new StringBuilder();
+
+    // for number account
+    numberAccounString.append(String.format("%06d", rand.nextInt(999999)));
+    numberAccounString.append(String.format("%05d", rand.nextInt(99999)));
+    // for key RIB
+    numberAccounString.append(" ");
+    numberAccounString.append(String.format("%02d", rand.nextInt(99)));
 
     return numberAccounString.toString();
 
