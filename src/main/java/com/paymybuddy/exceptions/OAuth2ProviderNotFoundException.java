5dafb31bab6c8722f3b75c1b7ad11af4e555508e
Delaval Dorian <delaval.htps@gmail.com>
2022-03-22 21:47:02 +0100

    update page  error
    - add ErrorAttributes to  custom error to have error message in page error
    - add @ResponseStatus to all exception to be handle by error page

--- src/main/java/com/paymybuddy/exceptions/OAuth2ProviderNotFoundException.java
+++ src/main/java/com/paymybuddy/exceptions/OAuth2ProviderNotFoundException.java
@@ -1,5 +1,9 @@
 package com.paymybuddy.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class OAuth2ProviderNotFoundException extends RuntimeException {
 
     public OAuth2ProviderNotFoundException(String message) {
