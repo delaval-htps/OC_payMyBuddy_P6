c655bd5b7cfdd2e73afacc12d898b0be573faea3
Delaval Dorian <delaval.htps@gmail.com>
2022-03-22 21:52:23 +0100

    update form profile
    - validation of form ok
    - bank Account registred for a new user

--- src/main/java/com/paymybuddy/controllers/ProfileController.java
+++ src/main/java/com/paymybuddy/controllers/ProfileController.java
@@ -1,46 +1,112 @@
 package com.paymybuddy.controllers;
 
 import java.util.Optional;
-import com.paymybuddy.model.ApplicationAccount;
+import javax.validation.Valid;
+import com.paymybuddy.dto.ApplicationAccountDto;
+import com.paymybuddy.dto.BankAccountDto;
+import com.paymybuddy.dto.UserDto;
+import com.paymybuddy.exceptions.UserNotFoundException;
 import com.paymybuddy.model.BankAccount;
-import com.paymybuddy.model.BankCard;
 import com.paymybuddy.model.User;
-import com.paymybuddy.service.ApplicationAccountService;
+import com.paymybuddy.service.BankAccountService;
 import com.paymybuddy.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+
 public class ProfileController {
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private UserService userService;
 
-    @Autowired
-    private ApplicationAccountService applicationAccountService;
+        @Autowired
+        private BankAccountService bankAccountService;
 
-    @GetMapping("/profile")
-    public String getProfil(Authentication authentication, Model model) {
-        Optional<User> user = userService.findByEmail(authentication.getName());
+        @Autowired
+        private ModelMapper modelMapper;
 
-        if (user.isPresent()) {
+        @GetMapping("/profile")
+        public String getProfil(Authentication authentication, Model model,
+                        RedirectAttributes redirectAttributes) {
+                Optional<User> user = userService.findByEmail(authentication.getName());
 
-            User currentUser = user.get();
-            model.addAttribute("user", currentUser);
-            Optional<ApplicationAccount> appAccount =
-                    applicationAccountService.findByEmail(currentUser.getEmail());
-            if (appAccount.isPresent()){
-                model.addAttribute("appAccount", currentUser.getApplicationAccount());
-            }
-           
-            BankAccount bankAccount = new BankAccount();
-            model.addAttribute("bankAccount", bankAccount);
-            
-            BankCard bankCard = new BankCard();
-            model.addAttribute("bankCard", bankCard);
+                if (user.isPresent()) {
+
+                        User currentUser = user.get();
+                        UserDto userDto = modelMapper.map(currentUser, UserDto.class);
+                        userDto.setFullName(currentUser.getFullName());
+                        model.addAttribute("user", userDto);
+
+                        model.addAttribute("applicationAccount",
+                                        modelMapper.map(currentUser.getApplicationAccount(),
+                                                        ApplicationAccountDto.class));
+
+                        BankAccountDto bankAccountDto = currentUser.getBankAccount() != null
+                                        ? modelMapper.map(currentUser.getBankAccount(),
+                                                        BankAccountDto.class)
+                                        : new BankAccountDto();
+                        model.addAttribute("bankAccount", bankAccountDto);
+
+                }
+                return "profile";
         }
-        return "profile";
-    }
+
+        @PostMapping("/profile/bankaccount")
+        public String createBankAccount(Model model,
+                        @Valid @ModelAttribute(value = "bankAccount") BankAccountDto bankAccountDto,
+                        BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                        Authentication authentication) {
+
+                Optional<User> user = userService.findByEmail(authentication.getName());
+
+                if (user.isPresent()) {
+
+                        User currentUser = user.get();
+
+                        if (bindingResult.hasErrors()) {
+                                model.addAttribute("user",
+                                                modelMapper.map(currentUser, UserDto.class));
+                                model.addAttribute("applicationAccount",
+                                                modelMapper.map(currentUser.getApplicationAccount(),
+                                                                ApplicationAccountDto.class));
+                                
+                                return "/profile";
+                        }
+
+                        BankAccount bankAccount =
+                                        modelMapper.map(bankAccountDto, BankAccount.class);
+                        currentUser.setBankAccount(bankAccount);
+
+                        BankAccount userBankAccount = bankAccountService.save(bankAccount);
+
+                        redirectAttributes.addFlashAttribute("success",
+                                        "your bank account was correctly registred.");
+
+                        UserDto userDto = modelMapper.map(currentUser, UserDto.class);
+                        userDto.setFullName(currentUser.getFullName());
+
+                        model.addAttribute("user", userDto);
+                        model.addAttribute("applicationAccount",
+                                        modelMapper.map(currentUser.getApplicationAccount(),
+                                                        ApplicationAccountDto.class));
+                        model.addAttribute("bankAccount",
+                                        modelMapper.map(userBankAccount, BankAccountDto.class));
+
+                        return "profile";
+
+                } else {
+                        throw new UserNotFoundException(
+                                        "this user with email " + authentication.getName()
+                                                        + " is not registred in application!");
+                }
+
+        }
 }
