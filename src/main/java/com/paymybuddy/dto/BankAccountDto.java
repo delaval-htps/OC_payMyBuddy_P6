9605e7f9ee90e6053a2c76772d045b855f9988f1
Delaval Dorian <delaval.htps@gmail.com>
2022-03-27 11:48:23 +0200

    cleancode

--- src/main/java/com/paymybuddy/dto/BankAccountDto.java
+++ src/main/java/com/paymybuddy/dto/BankAccountDto.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
