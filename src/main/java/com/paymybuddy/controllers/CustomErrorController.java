8c2cb3afb3878e465e10f0f9b56c82718b442aab
delaval-htps <delaval.htps@gmail.com>
2022-08-13 20:29:53 +0200

    refactor of error's page

--- src/main/java/com/paymybuddy/controllers/CustomErrorController.java
+++ src/main/java/com/paymybuddy/controllers/CustomErrorController.java
@@ -1,64 +1,54 @@
 package com.paymybuddy.controllers;
 
 import java.util.Map;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.ServletWebRequest;
+
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
 public class CustomErrorController implements ErrorController {
 
-    @Autowired
-    private ErrorAttributes errorAttributes;
+  private static final String LOG_MESSAGE =
+    "ErrorStatusCode: {} , errormessage: {} & errormessageURI: {}";
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        ServletWebRequest webRequest = new ServletWebRequest(request);
+  @Autowired
+  private ErrorAttributes errorAttributes;
 
-        Map<String, Object> eAttributes = this.errorAttributes.getErrorAttributes(webRequest,
-                ErrorAttributeOptions.defaults().including(ErrorAttributeOptions.Include.MESSAGE));
+  @RequestMapping("/error")
+  public String handleError(HttpServletRequest request, Model model) {
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    ServletWebRequest webRequest = new ServletWebRequest(request);
 
-        model.addAttribute("errorAttributes", eAttributes);
+    Map<String, Object> eAttributes = this.errorAttributes
+        .getErrorAttributes(webRequest,ErrorAttributeOptions.defaults()
+        .including(ErrorAttributeOptions.Include.MESSAGE));
 
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                log.error(("errormessage: {} & errormessageURI: {}"),
-                        request.getAttribute(RequestDispatcher.ERROR_MESSAGE),
-                        request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+    model.addAttribute("errorAttributes", eAttributes);
 
-                return "error-404";
+    if (status != null) {
+      Integer statusCode = Integer.valueOf(status.toString());
 
-            }
-            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                log.error(("errormessage: {} & errormessageURI: {}"),
-                        request.getAttribute(RequestDispatcher.ERROR_MESSAGE),
-                        request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
-                return "error-505";
+      log.error(LOG_MESSAGE,statusCode,
+        request.getAttribute(RequestDispatcher.ERROR_MESSAGE),
+        request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI)
+      );
 
-            }
-            if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                log.error(("errormessage: {} & errormessageURI: {}"),
-                        request.getAttribute(RequestDispatcher.ERROR_MESSAGE),
-                        request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
-                return "error-403";
-
-            }
-
-        }
-        return "error";
+      model.addAttribute("statusCode", statusCode);
+      
+      return "customError";
     }
-
-
+    return "error";
+  }
 }
