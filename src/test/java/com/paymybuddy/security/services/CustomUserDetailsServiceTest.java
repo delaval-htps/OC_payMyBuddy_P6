cfd4c0271d648c3538376884b7d2984c289a78d7
delaval-htps <delaval.htps@gmail.com>
2022-08-25 19:48:48 +0200

    modification customUserDetailServiceTest

--- src/test/java/com/paymybuddy/security/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/paymybuddy/security/services/CustomUserDetailsServiceTest.java
@@ -1,20 +1,22 @@
 package com.paymybuddy.security.services;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.paymybuddy.model.Role;
 import com.paymybuddy.repository.UserRepository;
 
