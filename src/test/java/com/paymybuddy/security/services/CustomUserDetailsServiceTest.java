ff4ed9c0b1738a4fb0b1eca85dc23bbfe97dcd42
Delaval Dorian <delaval.htps@gmail.com>
2022-08-25 12:57:10 +0200

    ut customUserDetailsService

--- src/test/java/com/paymybuddy/security/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/paymybuddy/security/services/CustomUserDetailsServiceTest.java
@@ -3,18 +3,19 @@ package com.paymybuddy.security.services;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-
+import java.util.HashSet;
 import java.util.Optional;
-
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import com.paymybuddy.model.Role;
 import com.paymybuddy.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +45,13 @@ public class CustomUserDetailsServiceTest {
         existedUser.setEmail("test@gmail.com");
         existedUser.setLastName("test_lastname");
         existedUser.setFirstName("testFirstname");
+        existedUser.setPassword("test");
+        Set<Role> roles = new HashSet<>();
+        Role role = new Role();
+        role.setName("USER_ROLE");
+        role.getUsers().add(existedUser);
+        roles.add(role);
+        existedUser.setRoles(roles);
 
         when(userRepository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(existedUser));
 
