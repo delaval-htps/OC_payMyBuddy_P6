d436673414c1360b405fc652bf560f12e3aee224
delaval-htps <delaval.htps@gmail.com>
2022-09-01 01:34:31 +0200

    change creation of number account for application using uuid

--- src/main/java/com/paymybuddy/UtilService.java
+++ src/main/java/com/paymybuddy/UtilService.java
@@ -1,11 +1,9 @@
 package com.paymybuddy;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,28 +19,10 @@ import lombok.Setter;
 @Setter
 public class UtilService {
 
-  private static Set<StringBuilder> accountNumbers = new HashSet<>();
 
-  public String getRandomApplicationAccountNumber() throws NoSuchAlgorithmException {
-
-    int lengthOfAccountNumbers = accountNumbers.size();
-    Random rand = SecureRandom.getInstanceStrong();
-    StringBuilder accountNumberString = new StringBuilder();
-
-    // as new generated account number is added to set , if set.size() > initial size then new
-    // account number is unique
-    while (accountNumbers.size() == lengthOfAccountNumbers) {
-
-      // for number account
-      accountNumberString.append(String.format("%06d", rand.nextInt(999999)));
-      accountNumberString.append(String.format("%05d", rand.nextInt(99999)));
-
-      // for key RIB
-      accountNumberString.append(" ");
-      accountNumberString.append(String.format("%02d", rand.nextInt(99)));
-      accountNumbers.add(accountNumberString);
-    }
-    return accountNumberString.toString();
-
+  public String getRandomApplicationAccountNumber()  {
+    
+    UUID uuid = UUID.randomUUID();
+    return uuid.toString();
   }
 }
