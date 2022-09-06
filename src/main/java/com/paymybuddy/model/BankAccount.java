580919c3fcf67d2b037bdde5437b7f969a78a239
Delaval Dorian <delaval.htps@gmail.com>
2022-09-06 04:10:23 +0200

    clean bank account

--- src/main/java/com/paymybuddy/model/BankAccount.java
+++ src/main/java/com/paymybuddy/model/BankAccount.java
@@ -7,10 +7,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import org.springframework.stereotype.Component;
 
