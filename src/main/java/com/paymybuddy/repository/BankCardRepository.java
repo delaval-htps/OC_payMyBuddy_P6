6a0129b355a8285ad7ed7bd93735102f4e52aafb
Delaval Dorian <delaval.htps@gmail.com>
2022-09-09 10:38:25 +0200

    add javadoc

--- src/main/java/com/paymybuddy/repository/BankCardRepository.java
+++ src/main/java/com/paymybuddy/repository/BankCardRepository.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Repository;
 import com.paymybuddy.model.BankCard;
 import com.paymybuddy.model.User;
 
+/**
+ * Repository extends {@link JpaRepository} to acces to {@link BankCard}
+ */
 @Repository
 public interface BankCardRepository extends CrudRepository<BankCard, Long> {
     @Query(value = "select bc from BankCard bc  join fetch bc.user u where u=?1")
--- src/main/java/com/paymybuddy/service/BankCardService.java
+++ src/main/java/com/paymybuddy/service/BankCardService.java
@@ -9,22 +9,44 @@ import com.paymybuddy.model.BankCard;
 import com.paymybuddy.model.User;
 import com.paymybuddy.repository.BankCardRepository;
 
+/**
+ * Class of Service for {@link BankCard}
+ */
 @Service
 public class BankCardService {
 
     @Autowired
     private BankCardRepository bankCardRepository;
 
+    /**
+     * retrieve a bankCard of user.
+     * 
+     * @param user the user that own the bank card
+     * @return Optional bankcard of the user if exist or a optional empty.
+     */
     public Optional<BankCard> findByUser(User user) {
         return bankCardRepository.findByUser(user);
     }
 
+    /**
+     * retrieve a bank card with its given id.
+     * 
+     * @param id the id of the reserch bank card
+     * @return Optional bankcard if exist or a optional empty.
+     */
     public Optional<BankCard> findById(Long id) {
         return bankCardRepository.findById(id);
     }
 
+    /**
+     * save a bank card if it's not null.
+     * 
+     * @param card the bank card to save
+     * @return the bankCard saved
+     * @throws IllegalArgumentException if bankCard is null.
+     */
     public BankCard save(BankCard card) {
         return bankCardRepository.save(card);
     }
- 
+
 }
