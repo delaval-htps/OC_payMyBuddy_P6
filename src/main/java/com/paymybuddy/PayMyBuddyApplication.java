1ee2214969b403a61c698919c3516022ad38f5fb
Delaval Dorian <delaval.htps@gmail.com>
2023-07-03 18:42:15 +0200

    update conf for tomcat server

--- src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -2,14 +2,24 @@ package com.paymybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 /**
  * Main class for application.
  */
 @SpringBootApplication
-public class PayMyBuddyApplication {
+public class PayMyBuddyApplication extends SpringBootServletInitializer {
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(PayMyBuddyApplication.class);
+  }
+  
   public static void main(String[] args) {
     SpringApplication.run(PayMyBuddyApplication.class, args);
   }
 
+
+  
 }
